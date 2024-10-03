e3576f8bdff0c8aad836bef81586cf0150214784
khaled07-ag <khaled-kk01@hotmail.com>
2024-10-03 19:16:16 +0300

    email & password

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {type: String, required: true},
+  name: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
   description: {type: String},
   recipes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Recipe'}],
   Cuisines: [{
