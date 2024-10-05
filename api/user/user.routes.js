6274445ec379814eb09828e51258ec48eff68d2f
khaled07-ag <khaled-kk01@hotmail.com>
2024-10-05 12:17:26 +0300

    debug

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 const {signup, signin, getUser, updateUser, deleteUser} = require('./user.controller');
 
 userRouter.post('/signup', signup);
-userRouter.post('/login', signin);
+userRouter.post('/signin', passport.authenticate('local', {session: false}), signin);
 userRouter.get('/:id', getUser);
 userRouter.put('/:id', updateUser);
 userRouter.delete('/:id', deleteUser);
