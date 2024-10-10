f9e16f9913accc8ca6753a37ebb0dd46a2183dd0
Khaled alshammari <eng.Khaled10@outlook.com>
2024-10-10 18:14:17 +0300

    populate

--- api/cuisine/cuisine.controller.js
+++ api/cuisine/cuisine.controller.js
@@ -1,61 +1,60 @@
 const Cuisine = require("../../models/cuisine");
 
 const createCuisine = async (req, res, next) => {
-    try {
-        if (req.file) {
-            req.body.cuisineImage = req.file.path;
-        }
-        const cuisine = await Cuisine.create(req.body);
-        return res.status(201).json({ data: cuisine });
-    } catch (error) {
-        next(error);
+  try {
+    if (req.file) {
+      req.body.cuisineImage = req.file.path;
     }
+    const cuisine = await Cuisine.create(req.body);
+    return res.status(201).json({ data: cuisine });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getCuisines = async (req, res, next) => {
-    try {
-        const cuisines = await Cuisine.find();
-        return res.status(200).json({ data: cuisines });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const cuisines = await Cuisine.find().populate("recipes");
+    return res.status(200).json({ data: cuisines });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getOneCuisine = async (req, res, next) => {
-    try {
-        const cuisine = await Cuisine.findById(req.params.id);
-        return res.status(200).json({ data: cuisine });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const cuisine = await Cuisine.findById(req.params.id).populate("recipes");
+    return res.status(200).json({ data: cuisine });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateCuisine = async (req, res, next) => {
-    try {
-        if (req.file) {
-            req.body.cuisineImage = req.file.path;
-        }
-        const cuisine = await Cuisine.findByIdAndUpdate(req.params.id, req.body);
-        return res.status(200).json({ data: cuisine });
-    } catch (error) {
-        next(error);
+  try {
+    if (req.file) {
+      req.body.cuisineImage = req.file.path;
     }
+    const cuisine = await Cuisine.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(200).json({ data: cuisine });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const deleteCuisine = async (req, res, next) => {
-    try {
-        const cuisine = await Cuisine.findByIdAndDelete(req.params.id);
-        return res.status(200).json({ data: cuisine });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const cuisine = await Cuisine.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ data: cuisine });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    createCuisine,
-    getCuisines,
-    getOneCuisine,
-    updateCuisine,
-    deleteCuisine,
+  createCuisine,
+  getCuisines,
+  getOneCuisine,
+  updateCuisine,
+  deleteCuisine,
 };
-
